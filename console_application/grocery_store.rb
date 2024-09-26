47dbc7fe8f440fba502ad70e0f0111a1df90bef1
kousalyapt <kousalya@sedintechnologies.com>
2024-09-26 18:30:44 +0530

    Update grocery_store.rb

--- console_application/grocery_store.rb
+++ console_application/grocery_store.rb
@@ -1,154 +1,189 @@
 class GroceryStore
+    VALID_DENOMINATIONS = [2000,500,200,100,50,20,10,5,2,1]
+
     def initialize()
-        @available_no_of_notes = Hash.new
+        @cash_box = Hash.new
 
         puts "Welcome!!!"
 
         puts "------------------------------------------"
 
-        puts "Enter the no.of 2000 ruppee notes"
-        @available_no_of_notes[2000] = gets.chomp.to_i
+        puts "Please enter the quantity of each denomination available in your store:"
 
-        puts "Enter the no.of 500 ruppee notes"
-        @available_no_of_notes[500] = gets.chomp.to_i
+        VALID_DENOMINATIONS.each do |denomination|
+        
+            @cash_box[denomination] = get_valid_quantity(denomination)
 
-        puts "Enter the no.of 200 ruppee notes"
-        @available_no_of_notes[200] = gets.chomp.to_i
-
-        puts "Enter the no.of 100 ruppee notes"
-        @available_no_of_notes[100] = gets.chomp.to_i
-
-        puts "Enter the no.of 50 ruppee notes"
-        @available_no_of_notes[50] = gets.chomp.to_i
-
-        puts "Enter the no.of 20 ruppee"
-        @available_no_of_notes[20] = gets.chomp.to_i
-
-        puts "Enter the no.of 10 ruppee"
-        @available_no_of_notes[10] = gets.chomp.to_i
-
-        puts "Enter the no.of 5 ruppee"
-        @available_no_of_notes[5] = gets.chomp.to_i
-
-        puts "Enter the no.of 2 ruppee coins"
-        @available_no_of_notes[2] = gets.chomp.to_i
-
-        puts "Enter the no.of 1 ruppee coins"
-        @available_no_of_notes[1] = gets.chomp.to_i
+        end
 
         puts "------------------------------------------"
     end
 
-    def change
+    def start_transaction
 
         loop do
+            puts "Please choose an action:"
             puts "1.Transaction"
             puts "2.Exit"
+            puts "------------------------------------------"
+
+            user_choice = gets.chomp.to_i
 
             puts "------------------------------------------"
 
-            option_to_proceed = gets.chomp.to_i
+            break if user_choice != 1
 
+            product_price = get_product_price
+
+            user_currency = get_user_currency
+
+            total_user_payment = user_currency&.sum
+
+            total_user_payment < product_price ? 
+            puts("The amount provided is insufficient to cover the product price. You need to pay Rs.#{ product_price - total_user_payment } more" ):
+            process_transaction(total_user_payment, product_price, user_currency)
+              
             puts "------------------------------------------"
-
-            break if option_to_proceed == 2
-
-            puts "Enter the product amount"
-            product_amount = gets.chomp.to_i
-
-            puts "Enter the user currency denominations"
-            currency_denominations = gets.chomp
-             
-            puts "------------------------------------------"
-
-            splitted_currency = currency_denominations.split(',')
-
-            given_currency = splitted_currency.map { |num| num.to_i}
-
-            unless validate_denominations?(given_currency)
-                puts "Enter valid denominations"
-                break
-            end
-
-            total_user_currency = given_currency.sum
-
-            if total_user_currency < product_amount
-                puts "Insufficient Amount"
-
-            elsif total_user_currency >= product_amount
-
-                balance_to_be_given = total_user_currency - product_amount
-                
-                if balance_to_be_given > total_amount + total_user_currency
-                    puts "No change. Please provide exact change!!!"
-
-                else
-                    given_currency.each {|currency| @available_no_of_notes[currency] += 1}
-
-                    @change_to_be_given = Hash.new
-
-                    @available_no_of_notes.each do |key, value|
-
-                        if balance_to_be_given == 0
-                            break
-                        end
-
-                        if balance_to_be_given / key != 0 && value > 0
-                            no_of_notes = balance_to_be_given / key
-        
-                            if(@available_no_of_notes[key] <= no_of_notes)
-                                @change_to_be_given[key] = @available_no_of_notes[key]
-                                balance_to_be_given = balance_to_be_given - (@available_no_of_notes[key]*key)
-                            else
-                                @change_to_be_given[key] = no_of_notes
-                                balance_to_be_given = balance_to_be_given - (no_of_notes*key)
-                            end
-                        end
-
-                    end
-
-                    if balance_to_be_given > 1
-                        puts "NO CHANGE. PLEASE PROVIDE EXACT CHANGE!!!"
-                        given_currency.each {|currency| @available_no_of_notes[currency] -= 1}
-                    else
-                        
-                        if @change_to_be_given.empty?
-                            puts "NO CHANGE TO BE GIVEN"
-                        else
-                            puts "CHANGE TO BE GIVEN: "
-                            @change_to_be_given.each do |key, value| 
-                            puts "#{key} : #{value}"
-                            @available_no_of_notes[key] -= value
-                            end
             
-                            if balance_to_be_given == 1
-                            puts "Give a one ruppee chocolate"
-                            end
-                        end
-                    end                
-                end
-                puts "------------------------------------------"
-                puts "------------------------------------------"
-                puts "AVAILABLE AMOUNT"
-                    @available_no_of_notes.each {|key,value| puts "#{key} : #{value}" }
-                    puts "TOTAL : #{total_amount}"
-                puts "------------------------------------------"
-
-            end
         end
     end
 
+    def get_valid_quantity( denomination )
+
+        quantity = 0
+
+        while quantity <= 0
+            begin 
+            puts "Enter the no.of Rs.#{denomination}:"
+            quantity = Integer(gets.chomp)
+
+            if quantity < 0 
+                puts "*Please enter a valid number."
+            else
+                break
+            end
+
+            rescue ArgumentError
+                puts "*Please enter a valid number."
+            end
+
+            
+        end
+        quantity
+    end
+
+    def get_product_price
+
+        product_price = 0
+
+        while product_price <= 0
+            puts "Enter the total price of the product:"
+            product_price = gets.chomp.to_i
+            puts "*Please enter a valid amount." if product_price <= 0
+        end
+
+        product_price
+
+    end
+
+    def get_user_currency
+
+        puts "Enter the denominations provided by the customer (comma-separated):"
+        user_currency = gets.chomp.split(',').map(&:to_i)
+             
+        puts "------------------------------------------"
+
+        return user_currency if validate_denominations?(user_currency)
+
+        puts "The denominations provided are not valid. Please try again."
+        start_transaction
+
+    end
+
     def total_amount
-        2000 * @available_no_of_notes[2000] + 500 * @available_no_of_notes[500] + 200* @available_no_of_notes[200] + 100 * @available_no_of_notes[100] + 50 * @available_no_of_notes[50] + 20 * @available_no_of_notes[20] + 10 * @available_no_of_notes[10] + 5 * @available_no_of_notes[5] + 2 * @available_no_of_notes[2] + @available_no_of_notes[1]
+        2000 * @cash_box[2000] + 500 * @cash_box[500] + 200* @cash_box[200] + 100 * @cash_box[100] + 50 * @cash_box[50] + 20 * @cash_box[20] + 10 * @cash_box[10] + 5 * @cash_box[5] + 2 * @cash_box[2] + @cash_box[1]
        
     end
 
+    def process_transaction(total_user_payment, product_price, user_currency)
+
+        balance = total_user_payment - product_price
+                
+        if balance > total_amount + total_user_payment
+            puts "Sorry, we cannot provide change for this transaction. Please provide exact payment."
+
+        else
+            user_currency.each {|currency| @cash_box[currency] += 1}
+
+            calculate_change(user_currency, balance)
+ 
+        end
+
+        puts "------------------------------------------"
+        puts "------------------------------------------"
+        puts "AVAILABLE AMOUNT IN THE CASHBOX"
+        @cash_box.each {|key,value| puts "Rs.#{key} : #{value}" }
+        puts "TOTAL : Rs.#{total_amount}"
+        puts "------------------------------------------"
+
+    end
+
+    def calculate_change(user_currency, balance)
+        @change_to_be_given = Hash.new
+
+                @cash_box.each do |key, value|
+
+                    break if balance == 0                    
+
+                    if balance / key != 0 && value > 0
+                        no_of_notes = balance / key
+        
+                        if(@cash_box[key] <= no_of_notes)
+                            @change_to_be_given[key] = @cash_box[key]
+                            balance = balance - (@cash_box[key]*key)
+                        else
+                            @change_to_be_given[key] = no_of_notes
+                            balance = balance - (no_of_notes*key)
+                        end
+                    end
+
+                end
+
+                display_change(balance, user_currency)
+            
+    end
+
     def validate_denominations?(denominations)
-        valid_denominations = [1,2,5,10,20,50,100,200,500,2000]
-        denominations.all? {|amount| valid_denominations.include?(amount)}
+        
+        denominations.all? {|amount| VALID_DENOMINATIONS.include?(amount)}
+    end
+
+    def display_change(balance, user_currency)
+        
+        if balance > 1
+            puts "Sorry, we cannot provide change for this transaction. Please collect your cash or provide exact payment."
+            revert_cash(user_currency)
+        else
+            print "Give a one ruppee chocolate." if balance == 1
+                         
+            if @change_to_be_given.empty?
+                puts "NO CHANGE TO BE GIVEN"
+            else
+                puts "CHANGE TO BE GIVEN: "
+                @change_to_be_given.each do |key, value| 
+                    puts "Rs.#{key} : #{value}"
+                    @cash_box[key] -= value
+                end               
+            end                   
+        end
+
+    end
+
+    def revert_cash(user_currency)
+        user_currency.each {|currency| @cash_box[currency] -= 1}
     end
     
 end
 
 grocery = GroceryStore.new
-grocery.change
+grocery.start_transaction
