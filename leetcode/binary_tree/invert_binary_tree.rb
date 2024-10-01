3f7b6c314a91e78fc05df0e73c3e4ff453ff5fea
kousalyapt <kousalya@sedintechnologies.com>
2024-10-01 18:27:45 +0530

    Create invert_binary_tree.rb

---
+++ leetcode/binary_tree/invert_binary_tree.rb
@@ -0,0 +1,9 @@
+def invert_tree(root)
+    return if root.nil?
+    temp =  root.left
+    root.left = root.right  
+    root.right = temp
+    invert_tree(root.left)
+    invert_tree(root.right)
+    root
+end
