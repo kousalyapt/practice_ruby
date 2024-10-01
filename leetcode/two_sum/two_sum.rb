3fbf9ad1411e5f640b56f293755b450de7cd32b4
kousalyapt <kousalya@sedintechnologies.com>
2024-10-01 16:30:25 +0530

    Create two_sum.rb

---
+++ leetcode/two_sum/two_sum.rb
@@ -0,0 +1,17 @@
+def two_sum(numbers, target)
+    left_index = 0
+    right_index = numbers.length - 1
+
+    while numbers[left_index] + numbers[right_index] != target
+        if numbers[left_index] + numbers[right_index] < target
+            left_index += 1
+        else
+            right_index -= 1
+        end
+    end
+
+    result = Array.new
+    result.push(left_index + 1)
+    result.push(right_index + 1)
+    result
+end
